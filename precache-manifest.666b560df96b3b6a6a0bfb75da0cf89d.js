8e33798508abc1664800231a5f891fa718d23d0c
wes-o <weso.turn@gmail.com>
2019-12-24 04:21:27 -0600

    Updates

---
+++ precache-manifest.666b560df96b3b6a6a0bfb75da0cf89d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35c01e7a3a0a8987db0dae02e7fa9c12",
+    "url": "/project-1-wes-o/index.html"
+  },
+  {
+    "revision": "93899c614c7d6744aaaa",
+    "url": "/project-1-wes-o/static/css/main.b66c9f14.chunk.css"
+  },
+  {
+    "revision": "93d8a8a32381baaa6100",
+    "url": "/project-1-wes-o/static/js/2.e6895ab5.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/project-1-wes-o/static/js/2.e6895ab5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "93899c614c7d6744aaaa",
+    "url": "/project-1-wes-o/static/js/main.3dc84226.chunk.js"
+  },
+  {
+    "revision": "940f3d2b6a13d0ec0a4d",
+    "url": "/project-1-wes-o/static/js/runtime-main.60816152.js"
+  }
+]);
